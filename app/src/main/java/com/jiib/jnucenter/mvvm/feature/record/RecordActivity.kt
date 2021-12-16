a927f14b0d1e5b8948571a11639e59cccb428e05
millionswillnd <gufari35@gmail.com>
2021-12-17 00:44:56 +0900

    RecordActivity.kt 뷰 작업

--- app/src/main/java/com/jiib/jnucenter/mvvm/feature/record/RecordActivity.kt
+++ app/src/main/java/com/jiib/jnucenter/mvvm/feature/record/RecordActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
@@ -21,6 +22,7 @@ class RecordActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_record)
 
         viewmodel = ViewModelProvider(this).get(RecordViewModel::class.java)
+
     }
 
 
